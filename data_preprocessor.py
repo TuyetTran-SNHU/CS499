62060a3435bd1ab67309b41734d39d83e6e40afb
TuyetTran-SNHU <tuyet.tran@snhu.edu>
2024-11-28 04:40:47 -0500

    Additional Documentation

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -216,8 +216,10 @@ def main():
 
     img = cv2.imread('../data/test.png', cv2.IMREAD_GRAYSCALE)
     img_aug = Preprocessor((256, 32), data_augmentation=True).process_img(img)
+
     plt.subplot(121)
     plt.imshow(img, cmap='gray')
+    
     plt.subplot(122)
     plt.imshow(cv2.transpose(img_aug) + 0.5, cmap='gray', vmin=0, vmax=1)
     plt.show()
